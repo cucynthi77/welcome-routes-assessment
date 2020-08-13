84d1728bce3cd328ce555fada93a378a733be51c
cynthia cummins <cynthiacummins88@gmail.com>
2020-08-13 11:13:16 -0400

    routes

---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Welcome(props) {
+    return (
+        <div className="Welcome">
+            Welcome {props.name}
+        </div>
+    );
+
+}
+
+
+
+
+
+
+export default Welcome;
